0fb5d01094f46b3581b18ba5fcd9d74f8db4c2f6
javierbarr1 <54829259+javierbarr1@users.noreply.github.com>
2019-09-25 18:13:24 -0600

    resumen
    resumen

---
+++ FinanzasUVGproyecto/Presupuestos.java
@@ -0,0 +1,24 @@
+/*
+ * @author Javier Barrios 
+ * @doc Interfaz Resumne
+ * 
+ */
+public class Presupuestos {
+	
+	public int totalPresupuesto(int uno, int dos, int tres, int cuatro, int cinco, int seis, int siete, int ocho, int nueve, int diez) {
+		int total;
+		
+		total = uno + dos + tres + cuatro + cinco + seis + siete + ocho + nueve + diez;
+		
+		return total;
+	}
+	
+	public int porcentajePresupuesto(int totalPresupuesto, int totalIngresos) {
+		double porcentaje;
+		totalIngresos += 1;
+		
+		porcentaje = (totalPresupuesto * 100) / totalIngresos; 
+;
+		return (int) Math.round(porcentaje) ;
+	}
+}
